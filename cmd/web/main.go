d85c1deaea401277948c7a2f1fcc27a1ab78e3c5
orololuwa <emmybest.ea@gmail.com>
2024-03-21 12:28:36 +0100

    feat: initial boilerplate

---
+++ cmd/web/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/orololuwa/bookings-app/internal/config"
+	"github.com/orololuwa/bookings-app/internal/driver"
+	"github.com/orololuwa/bookings-app/internal/handlers"
+	"github.com/orololuwa/bookings-app/internal/helpers"
+	"github.com/orololuwa/bookings-app/internal/models"
+	"github.com/orololuwa/bookings-app/internal/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
+
+// main is the main function
+func main() {
+	db, err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.SQL.Close()
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() (*driver.DB, error) {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	app.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	// Connect to databse
+	log.Println("Connecting to dabase")
+	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=test_connect user=orololuwa password=")
+	if err != nil {
+		log.Fatal("Cannot conect to database: Dying!", err)
+	}
+	log.Println("Connected to database")
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+		return nil, err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app, db)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
+
+	return db, nil
+}
